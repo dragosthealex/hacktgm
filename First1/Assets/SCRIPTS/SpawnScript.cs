1bfff8d1df1c57fda07627723cf77d7a4e07e434
Paul Chelarescu <paul-constantin.chelarescu@student.manchester.ac.uk>
2017-04-23 01:18:21 +0300

    random gen runner

---
+++ First1/Assets/SCRIPTS/SpawnScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnScript : MonoBehaviour {
+    public GameObject[] Obj;
+    public float spawnMin = 1f;
+    public float spawnMax = 2f;
+
+    // Use this for initialization
+    void Start () {
+        Debug.Log("Spawwwn");
+        Spawn();
+    }
+
+    void Spawn() {
+        Debug.Log("Helloo");
+        Instantiate(Obj[Random.Range(0, Obj.Length)], transform.position,
+                Quaternion.identity);
+        Invoke ("Spawn", Random.Range(spawnMin, spawnMax));
+    }
+}
